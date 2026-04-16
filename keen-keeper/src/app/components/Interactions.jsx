0b456d7208185290bc990a0cdcb595ae3d0aa854
JI-Siam <jafirislam10@gmail.com>
2026-04-16 16:02:58 +0600

    Functional Filtering Timeline

--- keen-keeper/src/app/components/Interactions.jsx
+++ keen-keeper/src/app/components/Interactions.jsx
@@ -1,14 +1,36 @@
 'use client'
 import { FriendContext } from '../context/FriendContext';
 import{useContext} from 'react'
+import {useState} from "react"
 import InteractionCard from './InteractionCard';
 
 const Interactions = () => {
     const {interactionList} = useContext(FriendContext) ;
+    const [filteredData, setFilteredData] = useState([]) ;
+   
+
+    const handleFilter = (filterType) => {
+        console.log(filterType) ;
+         const newData = interactionList.filter(interaction => interaction.type == filterType) ;
+         setFilteredData(newData); 
+
+         if(filterType === "All") setFilteredData([]) ;
+    }
+
     console.log(interactionList) ;
+    console.log(filteredData , "filtered data") ;
     return (
         <div >
-            {interactionList.map((interaction,index) => <InteractionCard key={index} friend={interaction}></InteractionCard>)}
+            <select defaultValue=""  className="select" onChange={(e)=> handleFilter(e.target.value)} >
+                <option disabled={true}>Filter timeline</option>
+                   <option value="All">All</option>
+                <option value="Call">Call</option>
+                <option value="Text">Text</option>
+                <option value="Video">Video</option>
+            </select>
+            {filteredData.length != 0 ? filteredData.map((interaction,index) => <InteractionCard key={index} friend={interaction}></InteractionCard>) : 
+            interactionList.map((interaction,index) => <InteractionCard key={index} friend={interaction}></InteractionCard>) }
+           
         </div>
     );
 };
--- keen-keeper/src/app/timeline/page.jsx
+++ keen-keeper/src/app/timeline/page.jsx
@@ -3,7 +3,7 @@ import Interactions from "../components/Interactions";
 const TimelinePage = () => {
     return (
         <div className="container mx-auto my-20">
-           <h1 className="text-2xl font-bold ">Timeline</h1>
+           <h1 className="text-2xl font-bold mb-10">Timeline</h1>
            <Interactions></Interactions>
         </div>
     );
